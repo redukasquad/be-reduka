bc49553165d5bab80854a43586a6eeda6083c1b9
taqin <misbahulmuttaqin395@gmail.com>
2026-02-14 21:18:40 +0700

    aff

--- database/entities/user_target_entity.go
+++ database/entities/user_target_entity.go
@@ -3,13 +3,13 @@ package entities
 import "gorm.io/gorm"
 
 type UserTarget struct {
-	gorm.Model
+    gorm.Model
 
-	UserID              uint `json:"userId" form:"userId" binding:"required"`
-	UniversityProgramID uint `json:"universityProgramId" form:"universityProgramId" binding:"required"`
-	Priority            int  `json:"priority" form:"priority" binding:"required,min=1"`
+    UserID              uint `json:"userId" form:"userId" binding:"required"`
+    UniversityMajorID   uint `json:"universityProgramId" form:"universityProgramId" binding:"required"` // ganti namanya agar jelas foreign key
+    Priority            int  `json:"priority" form:"priority" binding:"required,min=1"`
 
-	// relations
-	User              User              `json:"user,omitempty"`
-	Major 						UniversityMajor 	`json:"universityProgram,omitempty"`
+    // relations
+    User  User           `json:"user,omitempty"`
+    Major UniversityMajor `gorm:"foreignKey:UniversityMajorID;references:ID" json:"universityProgram,omitempty"`
 }
