765a259b347a6c6abcd66d32b16fb71f5342513e
taqin <misbahulmuttaqin395@gmail.com>
2026-02-15 21:37:17 +0700

    add

--- database/entities/user_target_entity.go
+++ database/entities/user_target_entity.go
@@ -9,13 +9,7 @@ type UserTarget struct {
     UniversityMajorID   uint `json:"universityProgramId" form:"universityProgramId" binding:"required"` // ganti namanya agar jelas foreign key
     Priority            int  `json:"priority" form:"priority" binding:"required,min=1"`
 
-<<<<<<< HEAD
     // relations
     User  User           `json:"user,omitempty"`
     Major UniversityMajor `gorm:"foreignKey:UniversityMajorID;references:ID" json:"universityProgram,omitempty"`
-=======
-	// relations
-	User              User              `json:"user,omitempty"`
-	Major 						UniversityMajor 	`json:"universityProgram,omitempty" gorm:"foreignKey:UniversityProgramID"`
->>>>>>> 48a3d2b (fix: GORM foreign key tags and route param conflict)
 }
