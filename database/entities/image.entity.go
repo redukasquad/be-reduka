b792a9a35e3f7f56d948d1be230b596ac3aa0d61
taqin <misbahulmuttaqin395@gmail.com>
2026-02-16 20:50:09 +0700

    add

--- database/entities/image.entity.go
+++ database/entities/image.entity.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type Image struct {
 	gorm.Model
 
-	URL   string `gorm:"type:text;not null;uniqueIndex"`
+	URL    string `gorm:"type:varchar(2048);not null;uniqueIndex"`
 	Fileid string `gorm:"type:varchar(255);not null"`
 }
